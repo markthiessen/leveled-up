2a08f426b16213aa1fdbe63f41b1d503f2be9236
Stephen Visser <visser.stephen@gmail.com>
2013-03-25 15:13:47 -0600

    Keep the scroll location of the page

--- chrome_extension/reload.js
+++ chrome_extension/reload.js
@@ -2,11 +2,15 @@
  	var _connectedToLeveledUpServer=false;
  if(!_connectedToLeveledUpServer){
 	 (function(){
+	 	window.scroll(0, window.localStorage.getItem('lastKnowScrollLocation'));
 		var ws = new WebSocket("ws://localhost:9797"); 
 		ws.onopen = function () { 
 		    console.log('Connected to leveledUp change notification server....');
 		    _connectedToLeveledUpServer=true;
 		};				 
-		ws.onmessage = function (evt) { window.location.reload(); };
+		ws.onmessage = function (evt) { 
+			window.localStorage.setItem('lastKnowScrollLocation', window.pageYOffset);
+			window.location.reload(); 
+		};
 	})();
 }
