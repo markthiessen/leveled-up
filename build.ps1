abe277154fd0bc8353ad1127d59cd2dcdd527b01
Curtis Badke <curtis@badkid.ca>
2013-03-30 18:48:14 -0600

    Separate chrome as a feature. This makes it an optional component in the future.

--- build.ps1
+++ build.ps1
@@ -5,7 +5,7 @@ $dir = Split-Path -parent $PSCommandPath
 & "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --pack-extension="$dir\chrome_extension\" --pack-extension-key="$dir\chrome_extension.pem"
 
 [System.Threading.Thread]::Sleep(1000)
-move "$dir\chrome_extension.crx" "$dir\chrome_extension\chrome_extension.crx"
+move -Force "$dir\chrome_extension.crx" "$dir\chrome_extension\chrome_extension.crx"
 
-& MSBuild.exe "$dir\msi\MSI.wixproj" /t:Rebuild /p:Configuration=Release /p:Platform="x64"
-copy "$dir\msi\bin\x64\Release\*.msi" "$dir\build\"
+& MSBuild.exe "$dir\Installer\Installer.wixproj" /t:Rebuild /p:Configuration=Release /p:Platform="x64"
+copy "$dir\Installer\bin\x64\Release\*.exe" "$dir\build\"
